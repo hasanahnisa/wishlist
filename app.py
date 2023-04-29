2215fae9d7e45b556cf79eada896de9c6ac642d5
Hasanah Nisa <64188572+hasanahnisa@users.noreply.github.com>
2023-04-29 12:03:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -41,20 +41,17 @@ def bucket_done():
         {'$set' : {'done' : 1 }}
     )
     return jsonify({'msg': 'update done!'})
+   
+@app.route("/delete", methods=["POST"])
+def delete_bucket():
+    num_receive = request.form['num_give']
+    db.bucket.delete_one({'num': int(num_receive)})
+    return jsonify({'msg': 'delete done!'})
 
 @app.route("/bucket", methods=["GET"])
 def bucket_get():
     buckets_list = list(db.bucket.find({}, {'_id' : False}))
     return jsonify({'buckets': buckets_list})
-
-@app.route("/delete", methods=["POST"])
-def bucket_delete():
-    num_receive = request.form['num_give']
-    db.bucket.delete_one(
-        {'num': int(num_receive)},
-        {'$set' : {'delete' : 0 }}
-        )
-    return jsonify({'msg': 'delete done!'})
 
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
